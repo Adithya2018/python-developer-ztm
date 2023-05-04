b9a68f1fe38bc415dc18e8249fffbb2c2c8fa6d6
Adithyan S Raj <adithyansraj20@gmail.com>
2023-05-04 10:27:18 +0530

    Update wizard_game.py

--- Advanced Python/Object Oriented Programming/wizard_game.py
+++ Advanced Python/Object Oriented Programming/wizard_game.py
@@ -1,17 +1,28 @@
 # OOP
 
 class PlayerCharacter:
+    # Class Object Attribute
+    # Static and exists for all objects
+    membership = True
+
     # Constructor
     # Special Method (Initialization)
     # Dunder or Magic Method
     def __init__(self, name, age):
-        # attributes
-        self.name = name
-        self.age = age
+        # Regular attributes
+        if (self.membership):
+            self.name = name
+            self.age = age
 
     def run(self):
         print('run')
         return 'done'
+
+    def shout(self):
+        print(f'My name is {self.name}')
+
+    def power_attack(self, power_level):
+        print(f'Attack with Power {power_level}')
 
 
 player1 = PlayerCharacter('Adi', 23)
@@ -22,3 +33,11 @@ player2.defense = 100
 print(player1.name, player1.age, player1.attack)
 print(player2.name, player2.age, player2.defense)
 
+print(player1.membership)
+print(player2.membership)
+
+# help(list)
+player1.shout()
+player2.shout()
+
+player1.power_attack(23)
