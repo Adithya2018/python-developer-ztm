19b9b6b7db6d95993db3b33968078c6ccf38af47
Adithyan S Raj <adithyansraj20@gmail.com>
2023-05-04 10:01:38 +0530

    multiple objects created

---
+++ Advanced Python/Object Oriented Programming/wizard_game.py
@@ -0,0 +1,22 @@
+# OOP
+
+class PlayerCharacter:
+    # Constructor
+    # Special Method (Initialization)
+    # Dunder or Magic Method
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def run(self):
+        print('run')
+        return 'done'
+
+
+player1 = PlayerCharacter('Adi', 23)
+player2 = PlayerCharacter('Druva', 21)
+player1.attack = 50
+player2.defense = 100
+
+print(player1.name, player1.age, player1.attack)
+print(player2.name, player2.age, player2.defense)
